0d23008bad58f4ea830230f080db088380f14b81
FERNANDO <fgolmos10@gmail.com>
2023-10-07 18:52:18 -0600

    YA QUEDO al 100

--- app/src/main/java/com/example/practica51_android/MainActivity.java
+++ app/src/main/java/com/example/practica51_android/MainActivity.java
@@ -2,78 +2,172 @@ package com.example.practica51_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button [] btns = new Button[9];
-    boolean jstatus;
-    int[]gamestate={2,2,2,2,2,2,2,2,2};
-    int[][]win={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},
-            {0,4,8},{2,4,6}};
+    Button [][] buttons= new Button[3][3];
+    boolean jugador1turn=true;
+    int round=0;
+    static int jugador1puntos=0;
+    static int jugador2puntos=0;
 
-    int rounds;
-    int jugador1,jugador2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btns[0]=findViewById(R.id.c1);
-        btns[1]=findViewById(R.id.c2);
-        btns[2]=findViewById(R.id.c3);
-        btns[3]=findViewById(R.id.c4);
-        btns[4]=findViewById(R.id.c5);
-        btns[5]=findViewById(R.id.c6);
-        btns[6]=findViewById(R.id.c7);
-        btns[7]=findViewById(R.id.c8);
-        btns[8]=findViewById(R.id.c9);
 
-        for(int i=0;i<btns.length;i++)
+
+        for(int i=0; i<3;i++)
         {
-            btns[i].setOnClickListener(this);
+            for(int j=0; j<3;j++)
+            {
+                String buttonID="b_"+i+j;
+                int resid= getResources().getIdentifier(buttonID,"id",getPackageName());
+                buttons[i][j] = findViewById(resid);
+                buttons[i][j].setOnClickListener(this);
+            }
         }
 
-        jugador1=0;
-        jugador2=0;
-        jstatus=true;
-        rounds=0;
+
+
+        boolean resetear=getIntent().getBooleanExtra("reiniciar",false);
+        if(resetear)
+        {
+            resetGame();
+        }
+
+
     }
 
     @Override
     public void onClick(View v)
     {
-        String btnid= v.getResources().getResourceEntryName(v.getId());
-        int gamestatepointer = Integer.parseInt(btnid.substring(btnid.length()-1,btnid.length()));
-        if(jstatus)
-        {
-            ((Button)v).setBackgroundResource(R.drawable.equis);
-            gamestate[gamestatepointer]=0;
+        if(!((Button) v).getText().toString().equals("")){
+            return;
         }
-        else
-        {
-            ((Button)v).setBackgroundResource(R.drawable.circle);
-            gamestate[gamestatepointer]=1;
+        if(jugador1turn){
+            ((Button) v).setText("x");
+            ((Button) v).setTextSize(50);
         }
-        rounds++;
-        if(checkWinner)
+        else{
+            ((Button) v).setText("o");
+            ((Button) v).setTextSize(50);
+        }
+        round++;
+
+        if(checkForWin())
         {
-            if(jstatus)
+            if(jugador1turn)
             {
-                jugador1++;
-                updatePlayerScore();
-
+                jugador1gana();
             }
-            else
-            {
-
+            else {
+                jugador2gana();
             }
+        } else if (round==9) {
+            draw();
+        }
+        else {
+            jugador1turn=!jugador1turn;
+        }
+    }
 
+    public boolean checkForWin(){
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
         }
 
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
+                return true;
+            }
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
+            return true;
+        }
+        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void jugador1gana(){
+        jugador1puntos++;
+        Toast.makeText(this,"Jugador 1 gana!",Toast.LENGTH_SHORT).show();
+
+        resetBoard();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("ganador", "jugador1");
+        bundle.putInt("j1", jugador1puntos);
+        bundle.putInt("j2", jugador2puntos);
+
+        Intent intent = new Intent(this, MainActivity3.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+    public void jugador2gana(){
+        jugador2puntos++;
+        Toast.makeText(this,"Jugador 2 gana!",Toast.LENGTH_SHORT).show();
+
+        resetBoard();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("ganador", "jugador2");
+        bundle.putInt("j1", jugador1puntos);
+        bundle.putInt("j2", jugador2puntos);
+
+        Intent intent = new Intent(this, MainActivity3.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+    }
+    public void draw(){
+        Toast.makeText(this,"Empate",Toast.LENGTH_SHORT).show();
+        resetBoard();
     }
 
 
+    public void resetBoard(){
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                buttons[i][j].setText("");
+            }
+        }
+        round=0;
+        jugador1turn=true;
+    }
+    public void resetGame()
+    {
+        jugador2puntos=0;
+        jugador1puntos=0;
+        resetBoard();
+
+    }
 }
--- app/src/main/java/com/example/practica51_android/MainActivity3.java
+++ app/src/main/java/com/example/practica51_android/MainActivity3.java
@@ -2,13 +2,61 @@ package com.example.practica51_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity3 extends AppCompatActivity {
-
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
+   TextView j1,j2,contrein;
+   Button botonreiniciar,botonregresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Bundle bundle = getIntent().getExtras();
+        botonreiniciar = (Button) findViewById(R.id.botonreiniciar);
+        botonregresar = (Button) findViewById(R.id.btnregresar);
+        botonregresar.setOnClickListener(this);
+        botonreiniciar.setOnClickListener(this);
+        j1= (TextView) findViewById(R.id.j1);
+        j2=(TextView) findViewById(R.id.j2);
+        contrein=(TextView) findViewById(R.id.contrein);
+        if (bundle != null) {
+            String ganador = bundle.getString("ganador");
+            int puntosjugador1 = bundle.getInt("j1");
+            int puntosjugador2 = bundle.getInt("j2");
+
+            j1.setText("Jugador 1: " + puntosjugador1);
+            j2.setText("Jugador 2: " + puntosjugador2);
+        }
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==R.id.botonreiniciar)
+        {
+            new CountDownTimer(6000, 1000)
+            {
+                public void onTick(long millisUntilFinished) {
+                    contrein.setText("Regresando en: " + millisUntilFinished / 1000);
+                }
+                public void onFinish() {
+                    Intent intent = new Intent(MainActivity3.this,MainActivity.class);
+                    intent.putExtra("reiniciar",true);
+                    startActivity(intent);
+                }
+            }.start();
+
+        } else if (v.getId()==R.id.btnregresar) {
+            Intent intent = new Intent(MainActivity3.this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
